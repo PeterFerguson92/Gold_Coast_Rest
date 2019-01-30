ba09c27ba4a7fa776444ca2f1e5e055fe210086e
peterf <peter.ferguson@rightmove.co.uk>
2019-01-30 17:21:47 +0000

    patch in progress

--- user/views.py
+++ user/views.py
@@ -4,6 +4,7 @@ from rest_framework import views, permissions, status
 from rest_framework.response import Response
 from .serializer import UserSerializer
 from .models import User
+from rest_framework.mixins import UpdateModelMixin
 
 
 class SpaUserLogoutAllView(views.APIView):
@@ -19,7 +20,7 @@ class SpaUserLogoutAllView(views.APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class UserRetrieveUpdateAPIView(views.APIView):
+class UserRetrieveAPIView(views.APIView):
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = UserSerializer
 
@@ -33,19 +34,21 @@ class UserRetrieveUpdateAPIView(views.APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class RetrieveUserByUsernameView(views.APIView):
+class UserUpdateAPIView(views.APIView,UpdateModelMixin):
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = UserSerializer
 
-    def get(self, request,email):
-        print(email)
+    def patch(self, request, userId , *args, **kwargs):
+        print(userId)
         try:
-            user = User.objects.get(email=email)
+            user = User.objects.get(id=userId)
+            print(user.first_name,user.email)
+            print(request.data['detail'])
+            print(request.data['value'])
+            print(request.data['id'])
         except User.DoesNotExist:
-
-            return Response("User with username/email: " + email + " Not Found", status=status.HTTP_404_NOT_FOUND)
-        serializer = UserSerializer(instance=user)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response("User with id: " + userId + " Not Found", status=status.HTTP_404_NOT_FOUND)
+        return Response(status=status.HTTP_200_OK)
 
 
 def jwt_response_payload_handler(token, user=None, request=None):
