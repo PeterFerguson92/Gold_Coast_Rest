5e854e431690eaecee6fdfbf5f246766337c32b8
jrferguson41@gmail.com <ferguson92>
2019-06-02 18:26:54 +0100

    quick change 2

--- gold_coast_rest/settings.py
+++ gold_coast_rest/settings.py
@@ -157,3 +157,11 @@ CORS_ORIGIN_ALLOW_ALL = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
