9a3ca74c2557434ea393d533f64c51ecf2b5a330
jrferguson41@gmail.com <ferguson92>
2019-06-20 20:45:26 +0100

    quick change for static files

--- gold_coast_rest/settings.py
+++ gold_coast_rest/settings.py
@@ -153,15 +153,13 @@ USE_TZ = True
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
+
 
 STATIC_URL = '/static/'
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
+# Add these new lines
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
